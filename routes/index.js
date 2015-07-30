228341c47a44948a2272d185b95e6f6104212985
Micha Kops <micha.kops@gmail.com>
2015-07-30 09:41:18 +0200

    Room interaction refactored.

--- routes/index.js
+++ routes/index.js
@@ -1,84 +1,96 @@
 var http = require('http');
 
 module.exports = function (app, addon) {
-  var hipchat = require('../lib/hipchat')(addon);
-  var basePath = '/wp-content/byTag.php?tag=';
-var port = process.argv[2] || 8080;
-var maxResults = process.argv[3] || 10;
-  var options = {
-	  host: 'www.hascode.com',
-	  path: '/',
-	  method: 'GET',
-	  headers: {
-		   'Content-Type': 'application/json'
-	   }
-	};
+    var hipchat = require('../lib/hipchat')(addon);
+    var basePath = '/wp-content/byTag.php?tag=';
+    var maxResults = 10;
+    var options = {
+        host: 'www.hascode.com',
+        path: '/',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
 
-	var oauthCred = [];
+    // Root route. This route will serve the `addon.json` unless a homepage URL is
+    // specified in `addon.json`.
+    app.get('/',
+        function (req, res) {
+            // Use content-type negotiation to choose the best way to respond
+            res.format({
+                // If the request content-type is text-html, it will decide which to serve up
+                'text/html': function () {
+                    res.redirect(addon.descriptor.links.homepage);
+                },
+                // This logic is here to make sure that the `addon.json` is always
+                // served up when requested by the host
+                'application/json': function () {
+                    res.redirect('/atlassian-connect.json');
+                }
+            });
+        }
+    );
 
-	// on-install hook
-	app.post('/install', function(req, res){
-		console.log('handling new plugin registration');
+    // This is an example route to handle an incoming webhook
+    app.post('/message',
+        addon.authenticate(),
+        function (req, res) {
+            var term = req.body.item.message.message.split(' ')[1];
+            console.log('searching for given term: ' + term);
 
+            var message = "<a href=\"http://www.hascode.com/\">hasCode.com</a> blog articles for given term: <b>&quot;" + term + "&quot;</b>";
+            options.path = basePath + term;
+            var clientRequest = http.request(options, function (clientResponse) {
+                var output = '';
+                clientResponse.setEncoding('utf8');
 
-		var oauthId = req.body.oauthId
-		console.log('storing oauth credentials for client with id: '+oauthId)
-		oauthCred[oauthId] = req.body.oauthSecret;
+                clientResponse.on('data', function (chunk) {
+                    output += chunk;
+                });
 
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.end();
-	});
+                clientResponse.on('end', function () {
+                    var hits = JSON.parse(output);
+                    var hitsNum = hits.length;
+                    console.log(hitsNum + ' results for term ' + term + ' found, max-results set to: ' + maxResults);
+                    message += ' (' + hitsNum + ' hit/s) <ul>';
+                    message += hits.slice(0, maxResults).reduce(function (prev, cur) {
+                        return createMessage(prev) + createMessage(cur);
+                    }, "");
+                    message += '</ul>';
+                    if (hits.length > maxResults) {
+                        message += '<b><a href="http://www.hascode.com/tag/' + term + '">Show all ' + hitsNum + ' results for &quot;' + term + '&quot;</a></b>'
+                    }
 
-	// on-message hook
-	app.post('/message', function(req, res){
-		console.log('new search request received');
-		console.dir(req.body);
-		
-		var term = req.body.item.message.message.split(' ')[1];
-		console.log('searching for given term: '+term);
-		
-		var message = "<a href=\"http://www.hascode.com/\">hasCode.com</a> blog articles for given term: <b>&quot;"+term+"&quot;</b>";
+                    hipchat.sendMessage(req.clientInfo, req.context.item.room.id, message)
+                        .then(function (data) {
+                            res.send(200);
+                        });
+                });
+            });
+            clientRequest.end();
+        }
+    );
 
-		options.path=basePath+term;
-		var clientRequest = http.request(options, function(clientResponse){
-			var output = '';
-			clientResponse.setEncoding('utf8');
+    // Notify the room that the add-on was installed
+    addon.on('installed', function (clientKey, clientInfo, req) {
+        hipchat.sendMessage(clientInfo, req.body.roomId, 'The ' + addon.descriptor.name + ' add-on has been installed in this room');
+    });
 
-			clientResponse.on('data', function (chunk) {
-				output += chunk;
-			});
-			
-			clientResponse.on('end', function() {
-				var hits = JSON.parse(output);
-				var hitsNum = hits.length;
-				console.log(hitsNum+' results for term '+term+' found, max-results set to: '+maxResults);
-				message+=' ('+hitsNum+' hit/s) <ul>';
-				message += hits.slice(0, maxResults).reduce(function(prev, cur){
-					return createMessage(prev) + createMessage(cur);
-				}, "");
-				message+='</ul>';
-				if(hits.length>maxResults){
-					message+='<b><a href="http://www.hascode.com/tag/'+term+'">Show all '+hitsNum+' results for &quot;'+term+'&quot;</a></b>'
-				}
-				res.writeHead(200, {'Content-Type': 'application/json'});
-				res.end(JSON.stringify({ "color": "green", "message": message, "notify": false, "message_format": "html" }));	
-			});
-		});
+    // Clean up clients when uninstalled
+    addon.on('uninstalled', function (id) {
+        addon.settings.client.keys(id + ':*', function (err, rep) {
+            rep.forEach(function (k) {
+                addon.logger.info('Removing key:', k);
+                addon.settings.client.del(k);
+            });
+        });
+    });
 
-		clientRequest.on('error', function(err) {
-			console.log('error: ' + err.message);
-			res.writeHead(200, {'Content-Type': 'application/json'});
-			message+=' request failed with message: <em>'+err.message+'</em>';
-			res.end(JSON.stringify({ "color": "green", "message": message, "notify": false, "message_format": "html" }));	
-		});
-
-		clientRequest.end();
-	});
-
-	function createMessage(hit){
-		if(typeof hit === 'string'){
-			return hit;
-		}
-		return '<li><a href=\"'+hit.url+'\">'+hit.title+'</a></li>';	
-	}
+    function createMessage(hit) {
+        if (typeof hit === 'string') {
+            return hit;
+        }
+        return '<li><a href=\"' + hit.url + '\">' + hit.title + '</a></li>';
+    }
 };
